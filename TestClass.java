ce7efca219ed433c9356eaf9cf5fad2a7753fe49
Norteyi <33091002+Norteyi@users.noreply.github.com>
2017-12-21 08:05:40 -0500

    arraylistpartial
    array

--- TestClass.java
+++ TestClass.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 /**
  * Write a description of class TestClass here.
  *
@@ -16,7 +17,13 @@ public class TestClass
        int dragonx;
        int dragonprevy=0;
        int dragony;
+       int dragonsize;
+       int dragonnum;
+       boolean dColor = false;
+       String[] colorList = {"blue","BLUE", "red", "RED", "GREEN", "green"};
+       ArrayList<Dragon> dragonList = new ArrayList<Dragon>();
        Color background;
+       Color dragonColor;
        
        DrawingPanel panel = new DrawingPanel(width,height);
        Graphics g = panel.getGraphics();
@@ -28,12 +35,50 @@ public class TestClass
        sky.drawScene(g);
        sky.drawGround(g);
        
-       do{
-           System.out.println("what do you want the x value to be");
-           dragonx = user.nextInt();
+       System.out.println("How many dragons do you want (1-3)");
+       dragonnum = user.nextInt();
+       for(int x = 1; x<= dragonnum; x++){
+           do{
+               System.out.println("what do you want the x starting coordinate to be");
+               dragonx = user.nextInt();
+            }
+            while(dragonprevx == dragonx);
+            dragonprevx = dragonx;
+       
+            do{
+                System.out.println("what do you want the y starting coordinate to be");
+                dragony = user.nextInt();
+            }
+            while(dragonprevy == dragony);
+            dragonprevy = dragony;
+       
+            do{
+                System.out.println("What size do you want your dragon to be 1 or 2");
+                dragonsize = user.nextInt();
+            }
+            while(dragonsize != 1 && dragonsize != 2);
+            
+            do{
+                dColor = false;
+                System.out.println("What color do you want your dragon to be? Blue Green or Red");
+                dColor = user.nextLine();
+                if(dColor = "blue" || "BLUE"){
+                    dragonColor = Color.blue;
+                    dColor = true;
+                }
+                
+                if(dColor = "red" || "RED"){
+                    dragonColor = Color.red;
+                    dColor = true;
+                }
+              
+                if(dColor = "green" || "green"){
+                    dragonColor = Color.blue;
+                    dColor = true;
+                }
+            }
+            while(dColor == false);
         }
-        while(dragonprevx == dragonx);
-          
        
        Dragon draggy = new Dragon(background);
        
